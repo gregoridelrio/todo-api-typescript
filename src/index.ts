85360a34c652cbea8d14b68c547cbd2e6e996ea3
Laurea Gregori <gregoridelrio@gmail.com>
2026-09-03 13:52:43 +0200

    feat: add todo routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import todoRoutes from "./routes/todo.routes.js";
 
 const app = express();
+
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
   res.send("Todo API is running");
