4bc9d0815615f0a92d5dec9818839fd7b74a8e80
Laurea Gregori <gregoridelrio@gmail.com>
2026-09-07 12:18:30 +0200

    feat: add Todo type

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,9 +1,11 @@
-export const getTodos = () => {
+import type { Todo } from "../types/todo.types.js";
+
+export const getTodos = (): Todo[] => {
   return [
     {
       id: 1,
       title: "Learn TypeScript",
-      completed: false
+      completed: false,
     }
   ];
 };
