504bda0b096e1cd77212e3491736b8eedb8855d1
Laurea Gregori <gregoridelrio@gmail.com>
2026-09-07 13:15:58 +0200

    feat: add in-memory todo storage

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,21 +1,30 @@
 import type { CreateTodoInput, Todo } from "../types/todo.types.js";
 
+let nextId = 2;
+
+const todos: Todo[] = [
+  {
+    id: 1,
+    title: "Learn TypeScript",
+    completed: false,
+    priority: "medium"
+  }
+];
+
 export const getTodos = (): Todo[] => {
-  return [
-    {
-      id: 1,
-      title: "Learn TypeScript",
-      completed: false,
-      priority: "medium"
-    }
-  ];
+  return todos;
 };
 
 export const createTodo = (data: CreateTodoInput): Todo => {
-  return {
-    id: 1,
+  const todo: Todo = {
+    id: nextId,
     title: data.title,
     completed: data.completed,
     priority: data.priority
   };
+
+  nextId++;
+  todos.push(todo);
+
+  return todo;
 };
